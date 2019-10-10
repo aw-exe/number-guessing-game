25a78fcafab4f85a87bcc52bd0c410cbe282b75c
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-10-09 21:01:23 -0500

    Added code content for the scoreboard in my javascript file. Defining wins, losses, and tries uning let, getElementById, and textContent.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,37 @@
-const turns=3; 
-const rounds=5;
+const result = document.getElementById("UserHint")
 
-// random number selection in JS using Math.Floor//
-const numberGuessed = document.getElementById("userGuess").value;
+let wins = 0;
+let losses = 0;
+let tries = 0;
 
-document.onkeyup = function(event) {
-  
-  let computerNumber = Math.floor(Math.random() * Math.floor(6));
+const winsText = document.getElementById("wins-text");
+const lossesText = document.getElementById("losses-text");
+const triesText = document.getElementById("tries-text")
+
+document.onkeyup = function (event) {
+
+  let computerNumber = Math.floor(Math.random() * Math.floor(11));
   const userGuess = event.key;
   console.log(userGuess);
   console.log(computerNumber);
 
+
+  if (userGuess > computerNumber) {
+    losses++;
+    result.innerHTML = 'Too High! Try Again!';
+  } else if (userGuess < computerNumber) {
+    losses++;
+    result.innerHTML = 'Too Low! Try Again!';
+  } if (userGuess == computerNumber) {
+    wins++
+    result.innerHTML = 'You Got It Right!!';
+  }
+
+
+  winsText.textContent = "wins: " + wins;
+  lossesText.textContent = "losses: " + losses;
+  triesText.textContent = "tries: " + tries;
+
 }
 
-// function enterGuess() {
 
-//   let userGuess = document.getElementById("userGuess").value;
-//   if(userGuess > randomNumber) {
-//   	result.innerHTML ='Too High! Try Again!';
-//     document.getElementById("submit").clear();
-//   }
-//   else if(userGuess < randomNumber) {
-//   	result.innerHTML ='Too Low! Try Again!';
-//     document.getElementById("submit").clear();
-//   }
-//   else if(userGuess == randomNumber) {
-//   	result.innerHTML ='You Got It Right!!';
-//     document.getElementById("submit").clear();
-
-//   }
